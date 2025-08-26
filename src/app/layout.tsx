3e7296c27b0d4fb5ae92807b6eddea9df1ab8bdb
Sumandeep Sahoo <139282575+SumandeepSahoo@users.noreply.github.com>
2025-08-26 11:58:52 +0530

    Import Suspense from React in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Icons } from '@/components/Icons'
 import SearchBar from '@/components/SearchBar'
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
